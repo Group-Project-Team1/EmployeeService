ea3f46ef43122789fe1e8e6b808412ac1cf41d0d
TengZhang <366205968@qq.com>
2023-03-03 11:24:10 -0500

    Add annotations for controller and service

--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -1,4 +1,15 @@
 package com.example.employeeservice.controller;
 
+import com.example.employeeservice.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class EmployeeController {
+    EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 }
--- src/main/java/com/example/employeeservice/service/EmployeeService.java
+++ src/main/java/com/example/employeeservice/service/EmployeeService.java
@@ -1,4 +1,9 @@
 package com.example.employeeservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+
+
 }
