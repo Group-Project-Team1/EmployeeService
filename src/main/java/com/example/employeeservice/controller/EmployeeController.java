890db67c877bf8fdcc85c998e1d4d362741844b6
lcl <71365212+l-cl3424@users.noreply.github.com>
2023-03-06 00:00:10 -0500

    corrected the project structure

--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -18,13 +18,8 @@ import java.util.List;
 //@Api(value = "EmployeeController RESTful endpoints")
 public class EmployeeController {
 
-    private final EmployeeService employeeService;
-
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
+    EmployeeService employeeService;
 
     // get -> find
     @GetMapping("/find")
